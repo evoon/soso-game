040c3ddcfe16d0ebb6144738a4910e10d679ce67
Evan <56698867+evoon@users.noreply.github.com>
2026-02-10 20:42:51 +0100

    full size

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 const collisionsMap = []
 for (let i = 0; i < collisions.length; i += 70) {
@@ -485,4 +485,10 @@ addEventListener('click', () => {
     audio.Map.play()
     clicked = true
   }
+})
+
+// Handle window resize
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
 })
