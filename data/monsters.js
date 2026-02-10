49695d634b4660d4e33cb02aed3cf7aa6d2b8386
Evan <56698867+evoon@users.noreply.github.com>
2026-02-10 20:45:26 +0100

    Hardcoded x,y fix

--- data/monsters.js
+++ data/monsters.js
@@ -1,8 +1,8 @@
 const monsters = {
   Emby: {
     position: {
-      x: 280,
-      y: 325
+      x: window.innerWidth * 0.27,  // 280/1024 ≈ 0.27
+      y: window.innerHeight * 0.56   // 325/576 ≈ 0.56
     },
     image: {
       src: './img/embySprite.png'
@@ -17,8 +17,8 @@ const monsters = {
   },
   Draggle: {
     position: {
-      x: 800,
-      y: 100
+      x: window.innerWidth * 0.78,   // 800/1024 ≈ 0.78
+      y: window.innerHeight * 0.17   // 100/576 ≈ 0.17
     },
     image: {
       src: './img/draggleSprite.png'
