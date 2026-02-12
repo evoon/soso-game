97efd977d8a2519d436eba148f08833656768fc5
Evan <56698867+evoon@users.noreply.github.com>
2026-02-12 23:41:16 +0100

    Fix

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ for (let i = 0; i < charactersMapData.length; i += 70) {
 console.log(charactersMap)
 
 const boundaries = []
-const scaleFactor = canvas.width / 1024
+const zoomLevel = 1.4
+const scaleFactor = (canvas.width / 1024) * zoomLevel
 const moveSpeed = 1.5 * scaleFactor
 const offset = {
-  x: -2500 * scaleFactor,
-  y: -1400 * scaleFactor
+  x: -2033 * scaleFactor,
+  y: -204 * scaleFactor
 }
 
 collisionsMap.forEach((row, i) => {
@@ -127,10 +128,11 @@ playerLeftImage.src = './img/playerLeft.png'
 const playerRightImage = new Image()
 playerRightImage.src = './img/playerRight.png'
 
+const playerScale = canvas.width / 1024
 const player = new Sprite({
   position: {
-    x: canvas.width / 2 - (192 / 4 / 2) * scaleFactor,
-    y: canvas.height / 2 - (68 / 2) * scaleFactor
+    x: canvas.width / 2 - (192 / 4 / 2) * playerScale,
+    y: canvas.height / 2 - (68 / 2) * playerScale
   },
   image: playerDownImage,
   frames: {
@@ -143,7 +145,7 @@ const player = new Sprite({
     right: playerRightImage,
     down: playerDownImage
   },
-  scale: scaleFactor
+  scale: playerScale
 })
 
 const background = new Sprite({
@@ -231,195 +233,30 @@ function animate(timestamp) {
 
   if (battle.initiated) return
 
-  // activate a battle
-  if (keys.z.pressed || keys.q.pressed || keys.s.pressed || keys.d.pressed) {
-    for (let i = 0; i < battleZones.length; i++) {
-      const battleZone = battleZones[i]
-      const overlappingArea =
-        (Math.min(
-          player.position.x + player.width,
-          battleZone.position.x + battleZone.width
-        ) -
-          Math.max(player.position.x, battleZone.position.x)) *
-        (Math.min(
-          player.position.y + player.height,
-          battleZone.position.y + battleZone.height
-        ) -
-          Math.max(player.position.y, battleZone.position.y))
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: battleZone
-        }) &&
-        overlappingArea > (player.width * player.height) / 2 &&
-        Math.random() < 0.5
-      ) {
-        // deactivate current animation loop
-        window.cancelAnimationFrame(animationId)
-
-        audio.Map.stop()
-        audio.initBattle.play()
-        audio.battle.play()
-
-        battle.initiated = true
-        gsap.to('#overlappingDiv', {
-          opacity: 1,
-          repeat: 3,
-          yoyo: true,
-          duration: 0.4,
-          onComplete() {
-            gsap.to('#overlappingDiv', {
-              opacity: 1,
-              duration: 0.4,
-              onComplete() {
-                // activate a new animation loop
-                initBattle()
-                animateBattle()
-                gsap.to('#overlappingDiv', {
-                  opacity: 0,
-                  duration: 0.4
-                })
-              }
-            })
-          }
-        })
-        break
-      }
-    }
-  }
-
   if (keys.z.pressed && lastKey === 'z') {
     player.animate = true
     player.image = player.sprites.up
-
-    checkForCharacterCollision({
-      characters,
-      player,
-      characterOffset: { x: 0, y: moveSpeed }
+    movables.forEach((movable) => {
+      movable.position.y += moveSpeed
     })
-
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: {
-            ...boundary,
-            position: {
-              x: boundary.position.x,
-              y: boundary.position.y + moveSpeed
-            }
-          }
-        })
-      ) {
-        moving = false
-        break
-      }
-    }
-
-    if (moving)
-      movables.forEach((movable) => {
-        movable.position.y += moveSpeed
-      })
   } else if (keys.q.pressed && lastKey === 'q') {
     player.animate = true
     player.image = player.sprites.left
-
-    checkForCharacterCollision({
-      characters,
-      player,
-      characterOffset: { x: moveSpeed, y: 0 }
+    movables.forEach((movable) => {
+      movable.position.x += moveSpeed
     })
-
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: {
-            ...boundary,
-            position: {
-              x: boundary.position.x + moveSpeed,
-              y: boundary.position.y
-            }
-          }
-        })
-      ) {
-        moving = false
-        break
-      }
-    }
-
-    if (moving)
-      movables.forEach((movable) => {
-        movable.position.x += moveSpeed
-      })
   } else if (keys.s.pressed && lastKey === 's') {
     player.animate = true
     player.image = player.sprites.down
-
-    checkForCharacterCollision({
-      characters,
-      player,
-      characterOffset: { x: 0, y: -moveSpeed }
+    movables.forEach((movable) => {
+      movable.position.y -= moveSpeed
     })
-
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: {
-            ...boundary,
-            position: {
-              x: boundary.position.x,
-              y: boundary.position.y - moveSpeed
-            }
-          }
-        })
-      ) {
-        moving = false
-        break
-      }
-    }
-
-    if (moving)
-      movables.forEach((movable) => {
-        movable.position.y -= moveSpeed
-      })
   } else if (keys.d.pressed && lastKey === 'd') {
     player.animate = true
     player.image = player.sprites.right
-
-    checkForCharacterCollision({
-      characters,
-      player,
-      characterOffset: { x: -moveSpeed, y: 0 }
+    movables.forEach((movable) => {
+      movable.position.x -= moveSpeed
     })
-
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: {
-            ...boundary,
-            position: {
-              x: boundary.position.x - moveSpeed,
-              y: boundary.position.y
-            }
-          }
-        })
-      ) {
-        moving = false
-        break
-      }
-    }
-
-    if (moving)
-      movables.forEach((movable) => {
-        movable.position.x -= moveSpeed
-      })
   }
 }
 //animate()
